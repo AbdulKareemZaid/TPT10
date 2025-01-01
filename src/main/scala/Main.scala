ea667fa5b746b51aef861b46285f73da81d6724a
Abdul-Kareem Zaid <s12113676@stu.najah.edu>
2025-01-01 19:16:45 +0200

    set up spark session in main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,18 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    // Create a SparkConf object
+    val conf = new SparkConf()
+      .setAppName("Twitter Stream Processing")
+      .setMaster("local[*]")
+    // Create a SparkSession using the SparkConf object
+    val spark = SparkSession.builder()
+      .config(conf)
+      .getOrCreate()
+
+    // Call the run method from StreamProcessing object
+    StreamProcessing.run(spark)
   }
 }
